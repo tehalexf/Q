0b692773366687aed4530534ce35c88cc9f9bb81
Alexander Fu <aafu@ucdavis.edu>
2015-12-22 23:14:46 -0800

    Added the heatmap

--- js/quartermaster.js
+++ js/quartermaster.js
@@ -26,7 +26,7 @@ if ($(window).width() < 767) {
 
 var derp = 8;
 var dummy = $("<div/>")
-
+var dummy2 = $("<div/>")
 
 for (var i = 0; i < 48; i++) {
   var use = 0;
@@ -47,13 +47,16 @@ for (var i = 0; i < 48; i++) {
 
 
     var temp = $("<div class='row row-date'><button id='timerow-" + String(i) + "' class='time-btn btn btn-default btn-block '>" + String(use) + String(sub) + tim + "</button></div>");
+    var temp2 = $("<td id='heatrow-" + String(i) + "' class='invis-table'><div class='heatmap-box'></div></td>");
 
     dummy.append(temp);
+    dummy2.append(temp2);
 }
+
 console.log(dummy.html())
 
 $('#testme').html(dummy.html());
-
+$('#top-heatmap').html(dummy2.html());
 
 $('.time-btn').on('click', function() {
   $('.time-btn').removeClass('active');
@@ -92,7 +95,9 @@ else
 console.log('#timerow' + String(d));
 $('.myelement').slick('slickGoTo',d, false);
 $('#timerow-' + String(d)).addClass('btn-currentday');
-
+$('#heatrow-' + String(d)).addClass('selected-box');
+var cw = $( '#heatrow-' + String(d) ).width();
+$( '#heatrow-' + String(d) ).css({'height': (cw+ 4) + 'px'});
 
 // $( ".test" ).each(function() {
 //   var cw = $( this ).width();
