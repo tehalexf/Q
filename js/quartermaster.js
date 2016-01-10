230e0c415817f129aa6bc6bc98e48cc5c0d6ecef
David G Lin <dglin@ucdavis.edu>
2016-01-09 17:41:23 -0800

    Tokenize the regex into array called Tokens

--- js/quartermaster.js
+++ js/quartermaster.js
@@ -1,5 +1,12 @@
 // var pat = new RegExp(/^([+-])[mtwhfsuMTWHFSU]+[\s\n]*$/g);
-var pat = new RegExp(/(^[+-])[mtwhfsuMTWHFSU]+[\s\n]+(0[1-9]|1[0-2]):(0[1-9]|[0-5][0-9]|60)[ap]m-(0[1-9]|1[0-2]):(0[1-9]|[0-5][0-9]|60)[ap]m$/);
+var pat = new RegExp(/(^[+-])([mtwhfsuMTWHFSU]|(0[1-9]|1[0-2])\/(0[1-9]|1[0-2])\/20(0[1-9]|1[0-5]))+[\s\n]+(0[1-9]|1[0-2]):(0[1-9]|[0-5][0-9]|60)[ap]m-(0[1-9]|1[0-2]):(0[1-9]|[0-5][0-9]|60)[ap]m$/);
+
+
+function tokenizeExpression(line, delimiter)
+{
+    var array = line.split(delimiter);
+    return array;
+}
 
 
 $(document).ready(function() {
@@ -8,10 +15,17 @@ $(document).ready(function() {
         if (pat.test(this.value)) {
             console.log('Hi, David')
             $(this).parent().removeClass('has-error');
+            var delimiter = new RegExp(/[\s\n]+/);
+            var tokens = tokenizeExpression(this.value, delimiter);
+            for(i = 0; i < tokens.length; i++){
+                console.log(tokens[i]);
+            }
         } else {
             console.log('MHAS VALUE')
             $(this).parent().addClass('has-error');
         }
+        // console.log('The original string is: "' )
+
     });
 
     var myString = "+mtw +mtHw";
