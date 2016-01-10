e4d4b764d4e6336d31b7386b3d7a6e4817745d00
David G Lin <dglin@ucdavis.edu>
2016-01-09 17:05:29 -0800

    worked on the regex

--- js/quartermaster.js
+++ js/quartermaster.js
@@ -1,18 +1,17 @@
-var pat = new RegExp(/^(([+-])[mtwhfsuMTWHFSU]+[\s\n]*)*$/g);
+// var pat = new RegExp(/^([+-])[mtwhfsuMTWHFSU]+[\s\n]*$/g);
+var pat = new RegExp(/(^[+-])[mtwhfsuMTWHFSU]+[\s\n]+(0[1-9]|1[0-2]):(0[1-9]|[0-5][0-9]|60)[ap]m-(0[1-9]|1[0-2]):(0[1-9]|[0-5][0-9]|60)[ap]m$/);
+
+
 $(document).ready(function() {
 
     $('#textarea').bind('input propertychange', function() {
         if (pat.test(this.value)) {
-            console.log('HAS VALUE')
+            console.log('Hi, David')
             $(this).parent().removeClass('has-error');
         } else {
             console.log('MHAS VALUE')
             $(this).parent().addClass('has-error');
         }
-        //     // if (this.value.length) {
-        //     //     $("#yourBtnID").show();
-        //     // }
-        // var re = new RegExp("^([a-z0-9]{5,})$");
     });
 
     var myString = "+mtw +mtHw";
